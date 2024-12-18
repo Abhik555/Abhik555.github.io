40d4b47197b55c92048f998fbba07d5d3cc70c3d
Abthedev <42204991+Abhik555@users.noreply.github.com>
2024-12-18 13:02:47 +0530

    basic setup

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -1,11 +1,12 @@
 import { BackgroundLines } from "@/components/ui/background-lines";
+  
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen bg-white dark:bg-black">
+         <main className="flex flex-col items-center justify-center w-screen h-screen bg-white dark:bg-black">
       <BackgroundLines>
         <div className="flex flex-col items-center justify-center w-full h-full p-5">
-          Test
+          Certificates
         </div>
       </BackgroundLines>
     </main>
